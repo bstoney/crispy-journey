a25b24f0790b663469baea18f1ac0cbead2526c0
Brad Stoney <stoneby@amazon.com>
2025-12-09 15:57:27 -0800

    Day 6 part 1

--- advent-of-code/c++/src/adventofcode/day06/day06.cpp
+++ advent-of-code/c++/src/adventofcode/day06/day06.cpp
@@ -2,28 +2,62 @@
 
 #include <string>
 #include <vector>
-#include <functional>
-#include <utility>
-#include <algorithm>
+#include <ranges>
+#include <format>
 
 #include "../utils.hpp"
 
 using namespace std;
 
-class Day06 : public AdventOfCodeSolution<int, int> {
+class Day06 : public AdventOfCodeSolution<long, long> {
 private:
-    typedef AdventOfCodeSolution<int, int> super;
+    typedef AdventOfCodeSolution<long, long> super;
 public:
-    int part1(vector<string> data) {
-        return 0;
+    long part1(vector<string> data) {
+        vector<vector<string>> input;
+        for (string item : data) {
+            vector<string> args;
+            for (string arg : utils::split(" ", item) | views::filter([](string a) { return !a.empty(); })) {
+                args.push_back(arg);
+            }
+
+            input.push_back(args);
+        }
+
+        vector<string> operators = input.back();
+
+        int problemCount = operators.size();
+        int argumentCount = input.size() - 1;
+
+        utils::debug("Problems: ", problemCount);
+        utils::debug("Arguments: ", argumentCount);
+
+        long total = 0;
+        for (int i = 0; i < problemCount; i++) {
+            string op = operators[i];
+            long value = stol(input[0][i]);
+            for (int j = 1; j < argumentCount; j++) {
+                if (op == "*") {
+                    value *= stol(input[j][i]);
+                }
+                else if (op == "+") {
+                    value += stol(input[j][i]);
+                }
+            }
+
+            utils::log("- ", operators[i], " ", value);
+            total += value;
+        }
+
+        return total;
     }
 
-    int part2(vector<string> data) {
+    long part2(vector<string> data) {
         return 0;
     }
 
     void solve() {
-        super::solve(6, 4277556, 0);
+        super::solve(6, 4277556, 3263827);
     }
 
 private:
